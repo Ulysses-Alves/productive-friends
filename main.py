1464a5a167b2a0577212290ac843d36671ff3fe0
Ulysses Alves <ulyssesalves@protonmail.com>
2025-06-18 20:40:31 -0300

    inital

---
+++ main.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def login():
+    return render_template('login.html')
+
+@app.route('/home', methods=['POST'])
+def home():
+    userStyle = request.form.get('userStyle')
+    return render_template('index.html', userStyle=userStyle)
